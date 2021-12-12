f5542ed202c28bb233bb498b652460f6a985c596
Giovanni Aguirre <giovanni.fi05@gmail.com>
2021-12-12 17:10:51 -0600

    Update target path validator tests

--- tests/web/test_validators.py
+++ tests/web/test_validators.py
@@ -8,11 +8,11 @@ from ddownloader.web.validators import safe_target_path
 def test_safe_target_path_not_string():
     mock_field = Mock()
 
-    mock_field.data = {'target_path': 45}
+    mock_field.data = {'relative_target_path': 45}
     with pytest.raises(ValidationError):
         safe_target_path(None, mock_field)
 
-    mock_field.data['target_path'] = []
+    mock_field.data['relative_target_path'] = []
     with pytest.raises(ValidationError):
         safe_target_path(None, mock_field)
 
@@ -20,11 +20,11 @@ def test_safe_target_path_not_string():
 def test_safe_target_path_empty():
     mock_field = Mock()
 
-    mock_field.data = {'target_path': ' '}
+    mock_field.data = {'relative_target_path': ' '}
     with pytest.raises(ValidationError):
         safe_target_path(None, mock_field)
 
-    mock_field.data['target_path'] = ''
+    mock_field.data['relative_target_path'] = ''
     with pytest.raises(ValidationError):
         safe_target_path(None, mock_field)
 
@@ -32,11 +32,11 @@ def test_safe_target_path_empty():
 def test_safe_target_path_double_dot():
     mock_field = Mock()
 
-    mock_field.data = {'target_path': '../System32/test.sh'}
+    mock_field.data = {'relative_target_path': '../System32/test.sh'}
     with pytest.raises(ValidationError):
         safe_target_path(None, mock_field)
 
-    mock_field.data = {'target_path': '/System32/test.sh'}
+    mock_field.data = {'relative_target_path': '/System32/test.sh'}
     with pytest.raises(ValidationError):
         safe_target_path(None, mock_field)
 
@@ -46,14 +46,14 @@ def test_safe_target_path():
 
     # Try with a valid paths, exception must not be raised
 
-    mock_field.data = {'target_path': 'System32/test.sh'}
+    mock_field.data = {'relative_target_path': 'System32/test.sh'}
     safe_target_path(None, mock_field)
 
-    mock_field.data = {'target_path': 'test.sh'}
+    mock_field.data = {'relative_target_path': 'test.sh'}
     safe_target_path(None, mock_field)
     
-    mock_field.data['target_path'] = 'nrhioervfhuoefrvuhgi452'
+    mock_field.data['relative_target_path'] = 'nrhioervfhuoefrvuhgi452'
     safe_target_path(None, mock_field)
 
-    mock_field.data['target_path'] = '9/8/7/6/5/4/3/'
+    mock_field.data['relative_target_path'] = '9/8/7/6/5/4/3/'
     safe_target_path(None, mock_field)
